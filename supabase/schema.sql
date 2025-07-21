80311220d0115a94e018b2b9d4f87eeb05d42614
Alex <alexktev@gmail.com>
2025-07-21 15:57:43 -0400

    Remove stuff from schema.sql

--- supabase/schema.sql
+++ supabase/schema.sql
@@ -1,27 +1,3 @@
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
-COMMENT ON SCHEMA "public" IS 'standard public schema';
-
-CREATE EXTENSION IF NOT EXISTS "pg_graphql" WITH SCHEMA "graphql";
-
-CREATE EXTENSION IF NOT EXISTS "pg_stat_statements" WITH SCHEMA "extensions";
-
-CREATE EXTENSION IF NOT EXISTS "pgcrypto" WITH SCHEMA "extensions";
-
-CREATE EXTENSION IF NOT EXISTS "supabase_vault" WITH SCHEMA "vault";
-
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA "extensions";
-
 CREATE TYPE "public"."device_type" AS ENUM (
     'solar_array',
     'battery',
@@ -48,8 +24,6 @@ CREATE TYPE "public"."theme" AS ENUM (
 );
 
 ALTER TYPE "public"."theme" OWNER TO "postgres";
-SET default_tablespace = '';
-SET default_table_access_method = "heap";
 
 CREATE TABLE IF NOT EXISTS "public"."battery_config" (
     "device_id" "uuid" NOT NULL,
@@ -307,104 +281,3 @@ ALTER TABLE "public"."house_load" ENABLE ROW LEVEL SECURITY;
 ALTER TABLE "public"."profiles" ENABLE ROW LEVEL SECURITY;
 
 ALTER TABLE "public"."solar_config" ENABLE ROW LEVEL SECURITY;
-ALTER PUBLICATION "supabase_realtime" OWNER TO "postgres";
-
-GRANT USAGE ON SCHEMA "public" TO "postgres";
-GRANT USAGE ON SCHEMA "public" TO "anon";
-GRANT USAGE ON SCHEMA "public" TO "authenticated";
-GRANT USAGE ON SCHEMA "public" TO "service_role";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-GRANT ALL ON TABLE "public"."battery_config" TO "anon";
-GRANT ALL ON TABLE "public"."battery_config" TO "authenticated";
-GRANT ALL ON TABLE "public"."battery_config" TO "service_role";
-
-GRANT ALL ON TABLE "public"."battery_state" TO "anon";
-GRANT ALL ON TABLE "public"."battery_state" TO "authenticated";
-GRANT ALL ON TABLE "public"."battery_state" TO "service_role";
-
-GRANT ALL ON TABLE "public"."devices" TO "anon";
-GRANT ALL ON TABLE "public"."devices" TO "authenticated";
-GRANT ALL ON TABLE "public"."devices" TO "service_role";
-
-GRANT ALL ON TABLE "public"."energy_flows" TO "anon";
-GRANT ALL ON TABLE "public"."energy_flows" TO "authenticated";
-GRANT ALL ON TABLE "public"."energy_flows" TO "service_role";
-
-GRANT ALL ON TABLE "public"."ev_charge_sessions" TO "anon";
-GRANT ALL ON TABLE "public"."ev_charge_sessions" TO "authenticated";
-GRANT ALL ON TABLE "public"."ev_charge_sessions" TO "service_role";
-
-GRANT ALL ON TABLE "public"."ev_config" TO "anon";
-GRANT ALL ON TABLE "public"."ev_config" TO "authenticated";
-GRANT ALL ON TABLE "public"."ev_config" TO "service_role";
-
-GRANT ALL ON TABLE "public"."grid_data" TO "anon";
-GRANT ALL ON TABLE "public"."grid_data" TO "authenticated";
-GRANT ALL ON TABLE "public"."grid_data" TO "service_role";
-
-GRANT ALL ON TABLE "public"."house_load" TO "anon";
-GRANT ALL ON TABLE "public"."house_load" TO "authenticated";
-GRANT ALL ON TABLE "public"."house_load" TO "service_role";
-
-GRANT ALL ON TABLE "public"."profiles" TO "anon";
-GRANT ALL ON TABLE "public"."profiles" TO "authenticated";
-GRANT ALL ON TABLE "public"."profiles" TO "service_role";
-
-GRANT ALL ON TABLE "public"."solar_config" TO "anon";
-GRANT ALL ON TABLE "public"."solar_config" TO "authenticated";
-GRANT ALL ON TABLE "public"."solar_config" TO "service_role";
-
-ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES TO "postgres";
-ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES TO "anon";
-ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES TO "authenticated";
-ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES TO "service_role";
-
-ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS TO "postgres";
-ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS TO "anon";
-ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS TO "authenticated";
-ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS TO "service_role";
-
-ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES TO "postgres";
-ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES TO "anon";
-ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES TO "authenticated";
-ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES TO "service_role";
-
-
-
-
-
-
-RESET ALL;
